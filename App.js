bdd095ee9d34a97e9ad6d726060070bae4c69981
정연수 <jys1015@hanyang.co.kr>
2019-11-01 17:06:57 +0900

    AppLoading added before loading todo end

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
 import ToDo from "./ToDo";
+import { AppLoading } from "expo";
 
 const { height, width } = Dimensions.get("window")
 
 export default class App extends React.Component {
   state = {
-    newToDo: ""
+    newToDo: "",
+    loadedToTos: false
   };
+
+  componentDidMount = () => {
+    this._loadToDos();
+  }
   render() {
-    const { newToDo } = this.state
+    const { newToDo, loadedToTos } = this.state
+
+    if (!loadedToTos) {
+      return <AppLoading/>
+    }
+    
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content"/>
@@ -34,6 +45,12 @@ export default class App extends React.Component {
   _controlNewToDo = text =>{
     this.setState({
       newToDo: text
+    })
+  }
+  
+  _loadToDos = () => {
+    this.setState({
+      loadedToTos : true
     })
   }
 }
